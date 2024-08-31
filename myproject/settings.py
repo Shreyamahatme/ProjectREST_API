9f3f9263c43a88aabe15a9a33d2969d4dd98504c
Shreyamahatme <98048362+Shreyamahatme@users.noreply.github.com>
2024-08-31 14:23:13 +0530

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -53,10 +53,15 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydatabase',
+        'USER': 'mydatabaseuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
